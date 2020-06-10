4039a1634fe59eb5bbb86f1b512f27b7e6b42735
Ivo Gurtner <ivo.gurtner@edu.tbz.ch>
2020-06-10 12:07:42 +0200

    Add my part

--- core/src/ch/noseryoung/advanceduekm335/AdvancedCourseM335.java
+++ core/src/ch/noseryoung/advanceduekm335/AdvancedCourseM335.java
@@ -4,24 +4,31 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class AdvancedCourseM335 extends ApplicationAdapter {
 	SpriteBatch batch;
+	SpaceShip spaceShip;
 	Texture img;
+	Sprite sprite;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		spaceShip = new SpaceShip();
 		img = new Texture("badlogic.jpg");
+		sprite = new Sprite(img);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		sprite.draw(batch);
+		spaceShip.render(batch);
 		batch.end();
 	}
 	
---
+++ core/src/ch/noseryoung/advanceduekm335/SpaceShip.java
@@ -0,0 +1,31 @@
+package ch.noseryoung.advanceduekm335;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class SpaceShip {
+
+    Texture img;
+    Sprite sprite;
+
+    public SpaceShip() {
+        img = new Texture("/../");
+        sprite = new Sprite();
+    }
+
+    public void render(SpriteBatch spriteBatch) {
+        if (Gdx.input.isTouched()) {
+            sprite.setX(Gdx.input.getX());
+            sprite.setY(Gdx.graphics.getHeight() - Gdx.input.getY());
+        }
+        sprite.setScale(0.5f);
+        sprite.draw(spriteBatch);
+
+    }
+
+    public void dispose() {
+        img.dispose();
+    }
+}
