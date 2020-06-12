33b8a6ac4d5cbcdb6eabc8943a7ae482cdfe0319
Sabrina Westhauser <sabrina.westhauser@edu.tbz.ch>
2020-06-12 13:11:29 +0200

    explosion funktion gemacht

--- core/src/ch/noseryoung/advanceduekm335/GameScreen.java
+++ core/src/ch/noseryoung/advanceduekm335/GameScreen.java
@@ -2,11 +2,16 @@ package ch.noseryoung.advanceduekm335;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.ArrayList;
+
+import javax.swing.KeyStroke;
 
 
 public class GameScreen implements Screen {
@@ -15,11 +20,14 @@ public class GameScreen implements Screen {
     private Sprite bgSprite;
     private SpaceShip spaceShip;
     private Asteroid aestorid;
-    private Explosion explosion;
 
     private Asteroid aestorid1;
     private Asteroid aestorid2;
     private Asteroid aestorid3;
+
+    private ArrayList<Explosion> explosions;
+    private float x, y;
+
 
     public GameScreen(Game parent){
         batch = new SpriteBatch();
@@ -27,11 +35,14 @@ public class GameScreen implements Screen {
         bgSprite = new Sprite(bgTexture);
         spaceShip = new SpaceShip();
         aestorid = new Asteroid();
-        explosion = new Explosion();
 
         aestorid1 = new Asteroid();
         aestorid2 = new Asteroid();
         aestorid3 = new Asteroid();
+
+        explosions = new ArrayList<>();
+        y = 15;
+        x = Gdx.graphics.getWidth() / 2 - spaceShip.getWidth() / 2;
     }
 
     @Override
@@ -44,17 +55,37 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(0,0,0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        //Shooting Code
+        if (Gdx.input.isTouched()){
+            explosions.add(new Explosion(x + 4));
+            explosions.add(new Explosion(x +  spaceShip.getWidth() - 4));
+        }
+
+        //Update Expplosions
+        ArrayList<Explosion> explosionsToRemove = new ArrayList<>();
+        for(Explosion explosion : explosions){
+            explosion.update(delta);
+            if(explosion.isRemove()){
+                explosionsToRemove.add(explosion);
+            }
+        }
+        explosions.removeAll(explosionsToRemove);
+
+
         batch.begin();
         bgSprite.draw(batch);
         batch.end();
 
         spaceShip.render(batch);
         aestorid.render(batch);
-        explosion.render(batch, spaceShip);
 
         aestorid1.render(batch);
         aestorid2.render(batch);
         aestorid3.render(batch);
+
+        for(Explosion explosion: explosions){
+            explosion.render(batch);
+        }
 
     }
 
